3049a71db60f68fe887aa698644a2d2b710416d3
Ryley Ziegler <73793678+rtziegl@users.noreply.github.com>
2024-01-11 18:25:39 -0500

    dark mode enabled with darkMode and dark:property

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html',],
+  darkMode: 'media',
   theme: {
     extend: {
       ringColor: {
         'default': '#93c5fd', // default focus ring color
         'error': '#f87171', // custom error focus ring color
-      }
+      },
+      colors: {
+        primary: '#F57C00',
+        primarylight: '#FF9800',
+        primarydark: '#E65100',
+      },
     },
   },
   plugins: [require('@tailwindcss/forms'),],
