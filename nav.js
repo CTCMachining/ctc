5f48607a8297ca327b28c350d172a453342ab428
Ryley Ziegler <73793678+rtziegl@users.noreply.github.com>
2024-01-20 14:13:36 -0500

    scrolling done, navbar/menu start

---
+++ nav.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    // Set the initial state to closed
+    mobileMenuButton.setAttribute('aria-expanded', 'false');
+    mobileMenu.hidden = true;
+
+    mobileMenuButton.addEventListener('click', function () {
+        const isExpanded = mobileMenuButton.getAttribute('aria-expanded') === 'true';
+
+        // Toggle the aria-expanded attribute on the button
+        mobileMenuButton.setAttribute('aria-expanded', !isExpanded);
+
+        // Toggle the hidden class on the mobile menu
+        mobileMenu.hidden = !mobileMenu.hidden;
+    });
+
+    // Add transition effects
+    mobileMenu.addEventListener('transitionend', function () {
+        // This event is triggered when the transition is complete
+        if (mobileMenu.hidden) {
+            // If the menu is hidden, remove the transition class
+            mobileMenu.classList.remove('transition', 'ease-out', 'duration-100');
+        } else {
+            // If the menu is visible, add the transition class
+            mobileMenu.classList.add('transition', 'ease-out', 'duration-100');
+        }
+    });
+
+    // Auto-scroll functionality
+    const scrollToSection = function (sectionId) {
+        const section = document.getElementById(sectionId);
+
+        const scrollPosition = section.offsetTop - document.querySelector('nav').offsetHeight + 70;
+
+
+        // Scroll to the calculated position
+        window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
+
+        // Close the mobile menu after clicking
+        mobileMenuButton.setAttribute('aria-expanded', 'false');
+        mobileMenu.hidden = true;
+    };
+
+    // Add click event listeners for menu items
+    document.getElementById('menu-who-we-are').addEventListener('click', function () {
+        scrollToSection('about-us-section');
+    });
+
+    document.getElementById('menu-what-we-do').addEventListener('click', function () {
+        scrollToSection('our-services-section');
+    });
+
+    document.getElementById('menu-contact-us').addEventListener('click', function () {
+        scrollToSection('contactForm contact-form-section');
+    });
+
+    document.getElementById('menu-who-we-are-mobile').addEventListener('click', function () {
+        scrollToSection('about-us-section');
+    });
+
+    document.getElementById('menu-what-we-do-mobile').addEventListener('click', function () {
+        scrollToSection('our-services-section');
+    });
+
+    document.getElementById('menu-contact-us-mobile').addEventListener('click', function () {
+        scrollToSection('contactForm contact-form-section');
+    });
+});
